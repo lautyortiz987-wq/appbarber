1059aa5312bdfa917bcc2367572224dcf53210ba
lautyortiz987-wq <lautyortiz987@gmail.com>
2026-05-05 01:47:31 -0300

    Update LucideIcon.js

--- LucideIcon.js
+++ LucideIcon.js
@@ -1,10 +1,33 @@
 window.LucideIcon = ({ name, size = 16, className = '' }) => {
-    const lib = window.LucideReact || {};
     const pascal = name
         .split('-')
         .map(w => w.charAt(0).toUpperCase() + w.slice(1))
         .join('');
-    const Comp = lib[pascal];
-    if (!Comp) return <span style={{ display: 'inline-block', width: size, height: size }} />;
-    return <Comp size={size} className={className} />;
+
+    const iconData = window.lucide && window.lucide[pascal];
+
+    if (!iconData) {
+        return <span style={{ display: 'inline-block', width: size, height: size }} />;
+    }
+
+    const [, , children] = iconData;
+
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width={size}
+            height={size}
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className={className}
+        >
+            {(children || []).map(([tag, childAttrs], i) =>
+                React.createElement(tag, { key: i, ...childAttrs })
+            )}
+        </svg>
+    );
 };
