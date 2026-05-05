f2525b84fa7ccd0971502c59b2e2b71c2f9b7947
lautyortiz987-wq <lautyortiz987@gmail.com>
2026-05-05 02:06:39 -0300

    Update LucideIcon.js

--- LucideIcon.js
+++ LucideIcon.js
@@ -1,63 +1,69 @@
 window.LucideIcon = ({ name, size = 16, className = '' }) => {
-    const pascal = name
-        .split('-')
-        .map(w => w.charAt(0).toUpperCase() + w.slice(1))
-        .join('');
+    const icons = {
+        'chevron-left': 'M15 18l-6-6 6-6',
+        'chevron-right': 'M9 18l6-6-6-6',
+        'chevron-down': 'M6 9l6 6 6-6',
+        'trending-up': 'M22 7L13.5 15.5L8.5 10.5L2 17M16 7h6v6',
+        'trending-down': 'M22 17L13.5 8.5L8.5 13.5L2 7M16 17h6v-6',
+        'bar-chart-3': 'M3 3v18h18M18 17V9M13 17V5M8 17v-3',
+        'bar-chart-2': 'M18 20V10M12 20V4M6 20v-6',
+        'arrow-up-right': 'M7 17L17 7M7 7h10v10',
+        'arrow-down-left': 'M17 7L7 17M17 17H7V7',
+        'scissors': 'M6 9a3 3 0 1 0 0-6 3 3 0 0 0 0 6zM6 21a3 3 0 1 0 0-6 3 3 0 0 0 0 6zM20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12',
+        'receipt': 'M4 2v20l2-1 2 1 2-1 2 1 2-1 2 1 2-1 2 1V2l-2 1-2-1-2 1-2-1-2 1-2-1-2 1-2-1zM16 8H8M16 12H8M12 16H8',
+        'shopping-bag': 'M6 2L3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4zM3 6h18M16 10a4 4 0 0 1-8 0',
+        'wallet': 'M21 12V7H5a2 2 0 0 1 0-4h14v4M21 12v5H5a2 2 0 0 0 0 4h16v-5M21 12H9',
+        'trash-2': 'M3 6h18M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2M10 11v6M14 11v6',
+        'loader-2': 'M21 12a9 9 0 1 1-6.219-8.56',
+        'user-plus': 'M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2M12 11a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM19 8v6M22 11h-6',
+        'user-minus': 'M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2M12 11a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM22 11h-6',
+        'phone': 'M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z',
+        'users': 'M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2M9 11a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM23 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75',
+        'search': 'M21 21l-6-6m2-5a7 7 0 1 1-14 0 7 7 0 0 1 14 0',
+        'award': 'M12 15a6 6 0 1 0 0-12 6 6 0 0 0 0 12zM8.21 13.89L7 23l5-3 5 3-1.21-9.12',
+        'dollar-sign': 'M12 1v22M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6',
+        'pie-chart': 'M21.21 15.89A10 10 0 1 1 8 2.83M22 12A10 10 0 0 0 12 2v10z',
+        'calendar': 'M3 4h18v18H3zM16 2v4M8 2v4M3 10h18',
+        'clock': 'M12 2a10 10 0 1 0 0 20A10 10 0 0 0 12 2zM12 6v6l4 2',
+        'plus': 'M12 5v14M5 12h14',
+        'x': 'M18 6L6 18M6 6l12 12',
+        'check': 'M20 6L9 17l-5-5',
+        'edit': 'M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z',
+        'save': 'M19 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11l5 5v11a2 2 0 0 1-2 2zM17 21v-8H7v8M7 3v5h8',
+        'log-out': 'M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4M16 17l5-5-5-5M21 12H9',
+        'menu': 'M3 12h18M3 6h18M3 18h18',
+        'bell': 'M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9M13.73 21a2 2 0 0 1-3.46 0',
+        'settings': 'M12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6zM19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1-2.83 2.83l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-4 0v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83-2.83l.06-.06A1.65 1.65 0 0 0 4.68 15a1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1 0-4h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 2.83-2.83l.06.06A1.65 1.65 0 0 0 9 4.68a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 4 0v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 2.83l-.06.06A1.65 1.65 0 0 0 19.4 9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 0 4h-.09a1.65 1.65 0 0 0-1.51 1z',
+        'home': 'M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2zM9 22V12h6v10',
+        'git-branch': 'M6 3v12M18 9a3 3 0 1 0 0-6 3 3 0 0 0 0 6zM6 21a3 3 0 1 0 0-6 3 3 0 0 0 0 6zM18 9a9 9 0 0 1-9 9',
+    };
 
-    const iconData = window.lucide && window.lucide[pascal];
+    const d = icons[name];
 
-    if (!iconData) {
+    if (!d) {
         return React.createElement('span', {
             style: { display: 'inline-block', width: size, height: size }
         });
     }
 
-    // Lucide puede exponer la función createIcons o datos crudos
-    // Intentamos llamarlo como función primero
-    if (typeof iconData === 'function') {
-        try {
-            const result = iconData();
-            if (result && result.innerHTML) {
-                return React.createElement('span', {
-                    dangerouslySetInnerHTML: { __html: result.innerHTML },
-                    style: { display: 'inline-flex', width: size, height: size }
-                });
-            }
-        } catch(e) {}
-    }
+    const paths = d.split('M').filter(Boolean).map((segment, i) =>
+        React.createElement('path', {
+            key: i,
+            d: 'M' + segment,
+            fill: 'none'
+        })
+    );
 
-    // Formato array: [tag, attrs, children]
-    if (Array.isArray(iconData)) {
-        const children = iconData[2] || [];
-        const svgChildren = children
-            .filter(c => Array.isArray(c))
-            .map(([tag, childAttrs], i) =>
-                React.createElement(tag, { key: i, ...childAttrs })
-            );
-
-        return React.createElement('svg', {
-            xmlns: 'http://www.w3.org/2000/svg',
-            width: size,
-            height: size,
-            viewBox: '0 0 24 24',
-            fill: 'none',
-            stroke: 'currentColor',
-            strokeWidth: 2,
-            strokeLinecap: 'round',
-            strokeLinejoin: 'round',
-            className: className
-        }, ...svgChildren);
-    }
-
-    // Formato objeto con toSvg
-    if (iconData.toSvg) {
-        return React.createElement('span', {
-            dangerouslySetInnerHTML: { __html: iconData.toSvg({ width: size, height: size, class: className }) },
-            style: { display: 'inline-flex', alignItems: 'center' }
-        });
-    }
-
-    return React.createElement('span', {
-        style: { display: 'inline-block', width: size, height: size }
-    });
+    return React.createElement('svg', {
+        xmlns: 'http://www.w3.org/2000/svg',
+        width: size,
+        height: size,
+        viewBox: '0 0 24 24',
+        fill: 'none',
+        stroke: 'currentColor',
+        strokeWidth: 2,
+        strokeLinecap: 'round',
+        strokeLinejoin: 'round',
+        className: className
+    }, ...paths);
 };
