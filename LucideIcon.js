80ca267e471fde6efb9f0a3dbdaba8c225eaa847
lautyortiz987-wq <lautyortiz987@gmail.com>
2026-05-05 02:13:03 -0300

    Update LucideIcon.js

--- LucideIcon.js
+++ LucideIcon.js
@@ -41,10 +41,19 @@ window.LucideIcon = ({ name, size = 16, className = '' }) => {
     const shapes = icons[name];
 
     if (!shapes) {
-        return React.createElement('span', {
-            style: { display: 'inline-block', width: size, height: size }
-        });
-    }
+return React.createElement('svg', {
+        xmlns: 'http://www.w3.org/2000/svg',
+        width: size,
+        height: size,
+        viewBox: '0 0 24 24',
+        fill: 'none',
+        stroke: 'currentColor',
+        strokeWidth: '2',
+        strokeLinecap: 'round',
+        strokeLinejoin: 'round',
+        className: className,
+        style: { display: 'inline-block', verticalAlign: 'middle', color: 'inherit' }
+    }, ...children);
 
     const children = shapes.map(([tag, attrs], i) =>
         React.createElement(tag, { key: i, ...attrs })
